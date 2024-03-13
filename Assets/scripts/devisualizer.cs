eeb6efb5b3571680d5283ae655b078012d5e89ee
ERT2827 <Btonkin5@gmail.com>
2024-03-13 14:38:28 +0200

    Added transparency system

--- Assets/scripts/devisualizer.cs
+++ Assets/scripts/devisualizer.cs
@@ -5,8 +5,28 @@ using UnityEngine;
 public class devisualizer : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) {
-        // if(other.gameObject.layer != "Ground"){
-        //     other.gameObject.GetComponent<Renderer>().material.color.a = 0.4f;
-        // }
+        if(other.gameObject.layer != 6){
+            Color col = other.gameObject.GetComponent<Renderer>().material.color;
+
+
+            col.a = 0.1f;
+
+            other.gameObject.GetComponent<Renderer>().material.color = col;
+
+        }
     }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject.layer != 6){
+            Color col = other.gameObject.GetComponent<Renderer>().material.color;
+
+
+            col.a = 1f;
+
+            other.gameObject.GetComponent<Renderer>().material.color = col;
+                    Debug.Log(other.name);
+
+        }
+    }
+    
 }
