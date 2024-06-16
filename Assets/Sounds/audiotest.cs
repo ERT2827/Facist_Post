33a384c72f5da7a8b17b1234ba0e25a4575a778c
Uyalrin <daniel.wolfbat@gmail.com>
2024-06-16 12:35:26 +0200

    background audio

---
+++ Assets/Sounds/audiotest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiotest : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.pitch = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
